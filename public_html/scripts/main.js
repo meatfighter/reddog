c0509424ff9f1ba71cef2c44d0b887f6cb25a168
Michael Birken <o__1@hotmail.com>
2023-10-29 13:10:08 -0400

    panels

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,20 +1,35 @@
+const Panels = {
+    RED: 'red-panel',
+    GREEN: 'green-panel',
+    BLUE: 'blue-panel'
+};
+
 async function downloadPanels() {
-    const panelNames = [ 
-        'red-panel', 
-        'green-panel', 
-        'blue-panel'
-    ];
-       
     try {
-        return await Promise.all(panelNames.map(name => fetch(`${name}.html`))
+        return await Promise.all(Object.values(Panels).map(name => fetch(`${name}.html`))
                 .map(promise => promise.then(response => response.text())));
     } catch (error) {        
         return [];
     }    
 }
 
+function handlePanels(panels) {
+    if (panels.length === 0) {
+        displayError();
+        return;
+    } 
+    
+    Object.keys(Panels).forEach((key, index) => Panels[key] = panels[index]);
+    
+    document.getElementById('main-content').innerHTML = Panels.GREEN;
+}
+
+function displayError() {
+    document.getElementById('main-content').innerHTML = 'Error!';
+}
+
 function init() {
-    downloadPanels().then(panels => document.getElementById('hello').innerHTML = panels.length);
+    downloadPanels().then(handlePanels);
 }
 
 document.addEventListener('DOMContentLoaded', init);
