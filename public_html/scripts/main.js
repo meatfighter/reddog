5e0d36d9506c085d3ce2e6c4f933d88f474c0e06
Michael Birken <o__1@hotmail.com>
2023-10-29 13:09:58 -0400

    panels

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,5 +1,20 @@
+async function downloadPanels() {
+    const panelNames = [ 
+        'red-panel', 
+        'green-panel', 
+        'blue-panel'
+    ];
+       
+    try {
+        return await Promise.all(panelNames.map(name => fetch(`${name}.html`))
+                .map(promise => promise.then(response => response.text())));
+    } catch (error) {        
+        return [];
+    }    
+}
+
 function init() {
-    document.getElementById('hello').innerHTML = 'Hello, World!';
+    downloadPanels().then(panels => document.getElementById('hello').innerHTML = panels.length);
 }
 
 document.addEventListener('DOMContentLoaded', init);
