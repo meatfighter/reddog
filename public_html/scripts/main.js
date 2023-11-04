ba9bae6b2c44bc7870f45387d54698168781a3b1
Michael Birken <o__1@hotmail.com>
2023-11-04 13:17:21 -0400

    card back

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,3 +1,100 @@
+class ArrayShuffler {
+    
+    #array;
+    #index;
+    #shuffleLength;
+    #shuffleThreshold;
+    
+    constructor(array, shuffleLength, shuffleThreshold) {
+        this.#array = array;
+        this.#index = array.length;
+        this.#shuffleLength = shuffleLength ? shuffleLength : array.length;
+        this.#shuffleThreshold = shuffleThreshold ? shuffleThreshold : array.length;
+    }
+
+    next() {
+        if (this.#index >= this.#shuffleThreshold) {
+            this.shuffle();
+            this.#index = 0;
+        }
+        return this.#array[this.#index++];
+    }
+
+    #shuffle() {
+        for (let i = this.#shuffleLength - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.#array[i], this.#array[j]] = [this.#array[j], this.#array[i]];
+        }
+    }
+}
+
+const winPhrases = new ArrayShuffler([
+    'Congratulations!',
+    'Winner, winner, chicken dinner!',
+    'You\'ve done it!',
+    'Winning hand!',
+    'Well played!',
+    'Lucky you!',
+    'You\'ve won!',
+    'You\'ve struck gold!',
+    'Bingo!',
+    'You\'re a winner!',
+    'Well done!',
+    'Keep it up!',
+    'You\'re on a roll!',
+    'A clear win!',
+    'Luck is on your side!',
+    'Nice play!',
+    'A win in the cards!',
+    'It\s a win!',
+    'You\re doing great!'
+]);
+
+const tiePhrases = new ArrayShuffler([
+    'Push.',
+    'Tie.',
+    'Draw.',
+    'It\s a push.',
+    'It\s a tie.',
+    'It\s a draw.',
+    'We have a push.',
+    'We have a tie.',
+    'We have a draw.',
+    'The game\s a tie.',
+    'The game\s a draw.',    
+    'No winners or losers.',    
+    'No losses or wins.',
+    'It\s a stand-off.',
+    'No change in balance.',
+    'Your bet is returned.',
+    'Your wager is returned.',
+    'Take back your bet.',
+    'Take back your wager.'
+]);
+
+const losePhrases = new ArrayShuffler([
+    'Better luck next time.',
+    'Unlucky hand.',
+    'Tough break.',
+    'It\'s all part of the game.',
+    'You\'ll get it next time.',
+    'Unfortunately, it\'s a loss.',
+    'Not your time.',
+    'It\'s all part of the thrill.',
+    'No worries, there\'s more to come.',
+    'Keep playing, luck can turn.',
+    'Hard luck.',
+    'More chances ahead.',
+    'Stay in. The game\'s not over yet.',
+    'Please try again.',
+    'Sorry.',
+    'Not in the cards.',
+    'Nope.',
+    'Perhaps next time.',
+    'It\s a loss.',
+    'Too bad.'
+]);
+
 const Panels = {
     LOADING: 'loading-panel',
     BET: 'bet-panel'
@@ -48,7 +145,7 @@ function handlePanels(panels) {
 function downloadCards() {
     document.getElementById('main-content').innerHTML = Panels.LOADING;
     
-    const ranks = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace' ];
+    const ranks = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace', 'back' ];
     const suits = [ 'clubs', 'diamonds', 'hearts', 'spades' ];
     
     const progressBar = document.getElementById('loading-progress');
@@ -63,6 +160,10 @@ function downloadCards() {
 
 function handleCards(cards) {
     document.getElementById('main-content').innerHTML = Panels.BET;
+    
+    document.getElementById('left-card').innerHTML = cards[52];
+    document.getElementById('middle-card').innerHTML = cards[1];
+    document.getElementById('right-card').innerHTML = cards[2];
 }
 
 function displayFatalError() {
